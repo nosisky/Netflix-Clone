0b3b7c75420fc72819c593b2fb083b6b0d94fec8
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2020-11-15 11:14:37 +0100

    Setup firebase deploment

---
+++ src/utils/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "d7e767ee6a35d744b5f38b492900c7df";
+
+
+const requests = {
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
